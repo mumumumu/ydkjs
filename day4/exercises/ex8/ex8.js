c06e1a43f0b9d53de261bb4d698d88fbefb2839a
Jeff Mu <jmu@singleplatform.com>
2015-04-16 16:05:14 -0400

    ex8

--- day4/exercises/ex8/ex8.js
+++ day4/exercises/ex8/ex8.js
@@ -1,42 +1,47 @@
-function Widget(width,height) {
-	this.width = width || 50;
-	this.height = height || 50;
-	this.$elem = null;
+var Widget = {
+    init: function(width, height) {
+        this.width = width || 50;
+        this.height = height || 50;
+        this.$elem = null;
+    },
+    render: function($where) {
+        if (this.$elem) {
+            this.$elem.css({
+                width: this.width + "px",
+                height: this.height + "px"
+            }).appendTo($where);
+        }
+    }
 }
 
-Widget.prototype.render = function($where){
-	if (this.$elem) {
-		this.$elem.css({
-			width: this.width + "px",
-			height: this.height + "px"
-		}).appendTo($where);
-	}
-};
+var Button = Object.create(Widget);
 
-function Button(width,height,label) {
+Button.initButton = function(width, height, label) {
 	// "super" constructor call
-	Widget.call(this,width,height);
+    this.init(width, height);
 	this.label = label || "Default";
 
 	this.$elem = $("<button>").text(this.label);
 }
-Button.prototype = Object.create(Widget.prototype);
 
-Button.prototype.render = function($where) {
+Button.renderButton = function($where) {
 	// "super" call
-	Widget.prototype.render.call(this,$where);
+    this.render($where);
 	this.$elem.click(this.onClick.bind(this));
-};
+}
 
-Button.prototype.onClick = function(evt) {
+Button.onClick = function(evt) {
 	console.log("Button '" + this.label + "' clicked!");
 };
 
 $(document).ready(function(){
 	var $body = $(document.body);
-	var btn1 = new Button(125,30,"Hello");
-	var btn2 = new Button(150,40,"World");
+    var btn1 = Object.create(Button);
+    var btn2 = Object.create(Button);
 
-	btn1.render($body);
-	btn2.render($body);
+    btn1.initButton(125, 30, "Hello");
+	btn2.initButton(150, 40, "World");
+
+	btn1.renderButton($body);
+	btn2.renderButton($body);
 });
