b379afdc8e015131646ff1a668d1f270f1607c52
Jeff Mu <jmu@singleplatform.com>
2015-04-17 14:08:27 -0400

    ex13

--- day5/exercises/ex13/ex13.js
+++ day5/exercises/ex13/ex13.js
@@ -17,47 +17,29 @@ function output(text) {
 	console.log(text);
 }
 
-// **************************************
-// The old-n-busted callback way
+//
 
 function getFile(file) {
-	fakeAjax(file,function(text){
-		fileReceived(file,text);
-	});
+    return new Promise(function(resolve) {
+        fakeAjax(file, resolve);
+    });
 }
-
-function fileReceived(file,text) {
-	// haven't received this text yet?
-	if (!responses[file]) {
-		responses[file] = text;
-	}
-
-	var files = ["file1","file2","file3"];
-
-	// loop through responses in order for rendering
-	for (var i=0; i<files.length; i++) {
-		// response received?
-		if (files[i] in responses) {
-			// response needs to be rendered?
-			if (responses[files[i]] !== true) {
-				output(responses[files[i]]);
-				responses[files[i]] = true;
-			}
-		}
-		// can't render yet
-		else {
-			// not complete!
-			return false;
-		}
-	}
-
-	output("Complete!");
-}
-
-// hold responses in whatever order they come back
-var responses = {};
 
 // request all files at once in "parallel"
-getFile("file1");
-getFile("file2");
-getFile("file3");
+var f1 = getFile("file1");
+var f2 = getFile("file2");
+var f3 = getFile("file3");
+
+f1
+.then(output)
+.then(function() {
+    return f2;
+})
+.then(output)
+.then(function() {
+    return f3;
+})
+.then(output)
+.then(function() {
+    output("Complete!");
+});
