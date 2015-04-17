1551f8436b78a2d70ce7af1bee307ab8a17b5818
Jeff Mu <jmu@singleplatform.com>
2015-04-17 09:23:55 -0400

    ex8

--- day4/exercises/ex8/ex8.js
+++ day4/exercises/ex8/ex8.js
@@ -17,31 +17,31 @@ var Widget = {
 var Button = Object.create(Widget);
 
 Button.initButton = function(width, height, label) {
-	// "super" constructor call
+    // "super" constructor call
     this.init(width, height);
-	this.label = label || "Default";
+    this.label = label || "Default";
 
-	this.$elem = $("<button>").text(this.label);
+    this.$elem = $("<button>").text(this.label);
 }
 
 Button.renderButton = function($where) {
-	// "super" call
+    // "super" call
     this.render($where);
-	this.$elem.click(this.onClick.bind(this));
+    this.$elem.click(this.onClick.bind(this));
 }
 
 Button.onClick = function(evt) {
-	console.log("Button '" + this.label + "' clicked!");
+    console.log("Button '" + this.label + "' clicked!");
 };
 
 $(document).ready(function(){
-	var $body = $(document.body);
+    var $body = $(document.body);
     var btn1 = Object.create(Button);
     var btn2 = Object.create(Button);
 
     btn1.initButton(125, 30, "Hello");
-	btn2.initButton(150, 40, "World");
+    btn2.initButton(150, 40, "World");
 
-	btn1.renderButton($body);
-	btn2.renderButton($body);
+    btn1.renderButton($body);
+    btn2.renderButton($body);
 });
